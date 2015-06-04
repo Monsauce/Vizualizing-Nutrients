8ed4934e1fc6cdb2dddf277c5a5ab9b654a7a07f
Kristina Enciso <kristina@wajam.com>
2015-06-03 20:40:02 -0400

    Adding global.R for csv data

--- NLA Viz/server.R
+++ NLA Viz/server.R
@@ -3,8 +3,8 @@ library(RCurl)
 library(ggplot2)
 library(lme4)
 library(data.table)
-NLA_MB.URL<-getURL("https://raw.githubusercontent.com/Monsauce/Vizualizing-Nutrients/master/NLAdataset.csv")
-NLA_MB<-read.csv(text=NLA_MB.URL)
+# NLA_MB.URL <- getURL("https://raw.githubusercontent.com/Monsauce/Vizualizing-Nutrients/master/NLAdataset.csv")
+# NLA_MB<-read.csv(text=NLA_MB.URL)
 
 # Renaming variables
 NLA_MB$MC <- NLA_MB$MCYST_TL_UGL
