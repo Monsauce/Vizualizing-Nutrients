11f020447399b8477e18bbb0f88aeb995e970f5e
Kristina Enciso <kristina@wajam.com>
2015-06-03 22:03:19 -0400

    Putting all data manipulations into global.R & changing plot width/height

--- NLA Viz/global.R
+++ NLA Viz/global.R
@@ -1,3 +1,15 @@
-#NLA_MB <- data.table(read.csv("C:/Users/kmenciso/Desktop/EPA_Visualization/Vizualizing-Nutrients/NLAdataset.csv"))
-NLA_MB.URL <- getURL("https://raw.githubusercontent.com/Monsauce/Vizualizing-Nutrients/master/NLAdataset.csv")
-NLA_MB<-read.csv(text=NLA_MB.URL)
+NLA_MB <- data.table(read.csv("C:/Users/kmenciso/Desktop/EPA_Visualization/Vizualizing-Nutrients/NLAdataset.csv"))
+# NLA_MB.URL <- getURL("https://raw.githubusercontent.com/Monsauce/Vizualizing-Nutrients/master/NLAdataset.csv")
+# NLA_MB<-read.csv(text=NLA_MB.URL)
+
+
+# Renaming variables
+NLA_MB$MC <- NLA_MB$MCYST_TL_UGL
+NLA_MB$CY <- NLA_MB$ALL.CYANOS
+
+# Transform skewed variables
+NLA_MB$log10MC <- log10(NLA_MB$MC)
+NLA_MB$ssCY <- (NLA_MB$CY)^0.25
+NLA_MB$log10NTL <- log10(NLA_MB$NTL)
+NLA_MB$log10PTL <- log10(NLA_MB$PTL)
+mod<-glmer.nb(round(NLA_MB$ssCY,0) ~ log10NTL + (log10NTL|ECO_NUTA), data=NLA_MB)
