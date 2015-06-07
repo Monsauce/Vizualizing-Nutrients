9d8ccd3c844a6214ff2600205e74b66dae2937e9
Kristina Enciso <kristina@wajam.com>
2015-06-06 22:18:59 -0400

    Changing font size

--- NLAViz/ui.R
+++ NLAViz/ui.R
@@ -7,7 +7,7 @@ library(data.table)
 shinyUI(fluidPage(
   tags$style("body {background-color: #74B7E4; }"),
   tags$head(tags$style("#intro {color: black;
-                                font-size: 24px;
+                                font-size: 20px;
                                 font-face: 'arial bold';
                               }")),
   tags$head(tags$style("#question {color: black;
